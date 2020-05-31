510df05e8e0520459a1ec16cabde2828d6c83a5e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-05-31 16:02:15 -0400

    update gatsby config and bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Andrei Dobrinski's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`
+      name: `Andrei Dobrinski`,
+      summary: `— a front-end developer that loves the modern web.`
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog about modern web technology.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`
+      twitter: `andreidobrinski`
     }
   },
   plugins: [
