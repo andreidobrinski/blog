7c5a4fd786099b557813726792f908a9907796ae
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-09-27 23:24:14 -0400

    Updates

---
+++ component---src-pages-404-js-7c2d95aa4f483a32d22f.js
@@ -0,0 +1,2 @@
+(self.webpackChunkandrei_dobrinski_blog=self.webpackChunkandrei_dobrinski_blog||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var a=n(7294),i=n(3241),l=n(6179);e.default=function(t){var e=t.data,n=t.location,o=e.site.siteMetadata.title;return a.createElement(i.Z,{location:n,title:o},a.createElement(l.Z,{title:"404: Not Found"}),a.createElement("h1",null,"Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7c2d95aa4f483a32d22f.js.map
